a7783db1a94a97b670cd4bc04f3f38697d8b6b5d
salinaj27 <salinajohn80@gmail.com>
2022-05-13 17:42:33 -0400

    Added Token to userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/userModel");
 
@@ -10,25 +11,25 @@ const User = require("../models/userModel");
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
-  //Validation
+  // Validation
   if (!name || !email || !password) {
     res.status(400);
-    throw new Error("PLease Include All Fields...");
+    throw new Error("Please include all fields");
   }
 
-  //Find if user already exists
+  // Find if user already exists
   const userExists = await User.findOne({ email });
 
   if (userExists) {
     res.status(400);
-    throw new Error("User Already Exist");
+    throw new Error("User already exists");
   }
 
-  //Hash password
+  // Hash password
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
 
-  //create User
+  // Create user
   const user = await User.create({
     name,
     email,
@@ -40,10 +41,11 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
-    throw new Error("Invalid user data");
+    throw new error("Invalid user data");
   }
 });
 
@@ -56,12 +58,13 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const user = await User.findOne({ email });
 
-  //check user and passwords match
+  // Check user and passwords match
   if (user && (await bcrypt.compare(password, user.password))) {
     res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
@@ -69,6 +72,13 @@ const loginUser = asyncHandler(async (req, res) => {
   }
   res.send("Login Route");
 });
+
+//Generate token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = {
   registerUser,
