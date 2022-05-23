c32aa465fb9dfdb43b87414a983a5a7f7b80193d
salinaj27 <salinajohn80@gmail.com>
2022-05-23 17:04:18 -0400

    added ticket model and setup the routes

---
+++ backend/controllers/ticketController.js
@@ -0,0 +1,17 @@
+const async = require("express-async-handler");
+
+const User = require("../models/userModel");
+const Ticket = require("../models/ticketModel");
+
+const getTickets = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "getTickets" });
+});
+
+const createTicket = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "createTicket" });
+});
+
+module.exports = {
+  getTickets,
+  createTicket,
+};
