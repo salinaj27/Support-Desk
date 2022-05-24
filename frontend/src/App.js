c88ffd69b02a7c91cdf12f5915b4a73ed408a03a
salinaj27 <salinajohn80@gmail.com>
2022-05-24 08:55:50 -0400

    added a slice and service for tickets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 import NewTicket from "./pages/NewTicket";
 
 function App() {
@@ -17,7 +18,9 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/new-ticket" element={<NewTicket />} />
+            <Route path="/new-ticket" element={<PrivateRoute />}>
+              <Route path="/new-ticket" element={<NewTicket />} />
+            </Route>
           </Routes>
         </div>
       </Router>
